bba4d50f419d0725fd908e34576a2c558b26ba43
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-06-10 16:36:57 +0200

    Reverse an integer challenge completed

--- index.js
+++ index.js
@@ -18,3 +18,12 @@ function isPalindrome(str) {
   return str.toLowerCase().split('').reverse().join('') === str;
 }
 console.log(isPalindrome('heal'));
+
+// CHALLENGE 3: REVERSE AN INTEGER
+// Return an integer in reverse
+// ex. reverseInt(521) === 125
+
+function reverseInt(int) {
+  return String(int).split('').reverse().join('');
+}
+console.log(reverseInt(521));
