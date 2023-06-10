28990346842c373412e45fb80673c35225acfa6e
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-06-10 16:26:30 +0200

    reverseString() challenge completed

--- index.js
+++ index.js
@@ -3,3 +3,10 @@
 // CHALLENGE 1: REVERSE A STRING
 // Return a string in reverse
 // ex. reverseString('hello) === 'olleh
+
+function reverseString(str) {
+  str = str.split('').reverse().join('');
+  return str;
+}
+
+reverseString('hello');
